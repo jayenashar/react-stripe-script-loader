d4cd0ef3353f5beb6149d4b0dc3cf156f15dd73d
Jayen Ashar <jayenashar@users.noreply.github.com>
2021-06-18 10:03:35 +1000

    in case a second instance is mounted while the first is loading

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from 'react'
 
-const StripeScriptLoader = ({
-  children,
-  uniqueId,
-  script,
-  loader = 'Loading...',
-}) => {
+const StripeScriptLoader = ({ children, uniqueId, script, loader }) => {
   const [stripeLoaded, setStripeLoaded] = useState({})
   useEffect(() => {
     const loadScript = (src, uniqueId) =>
       new Promise((resolve, reject) => {
+        /** @type HTMLScriptElement */
         const scriptElement = document.getElementById(uniqueId)
 
         if (!scriptElement) {
@@ -17,7 +13,10 @@ const StripeScriptLoader = ({
           script.src = src
           script.id = uniqueId
 
-          const handleLoadScriptSuccess = () => resolve({ successful: true })
+          const handleLoadScriptSuccess = () => {
+            script.isLoaded = true;
+            resolve({successful: true});
+          }
           const handleLoadScriptFail = event => reject({ error: event })
 
           script.addEventListener('load', handleLoadScriptSuccess, {
@@ -25,6 +24,11 @@ const StripeScriptLoader = ({
           })
           script.addEventListener('error', handleLoadScriptFail, { once: true })
           document.head.appendChild(script)
+        } else if (!scriptElement.isLoaded) {
+          const handleLoadScriptSuccess = () => resolve({successful: true})
+          scriptElement.addEventListener('load', handleLoadScriptSuccess, {
+            once: true,
+          })
         } else {
           resolve({ successful: true })
         }
@@ -34,7 +38,7 @@ const StripeScriptLoader = ({
       setStripeLoaded(result)
     }
     fetchData()
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+  }, [])
 
   return stripeLoaded.successful ? children : loader
 }
